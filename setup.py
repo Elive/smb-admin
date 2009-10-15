677bcee480bf7d27b2ba08d1397a8902c1f67818
LinuxNIT <davewhb@gmail.com>
2009-10-15 01:09:20 +0000

    smb-admin, removed last uses of os.popen for compliance with newer python versions

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name = "smb-admin",
-	version = "1.08.02",
+	version = "1.08.03",
 	description = "This program was designed to assist users in managing samba settings, shares and users. It's purpose it to be simple yet allow the user plenty of options. For a more powerful samba admin program, look at gsamabd or SWAT.",
 	author = "David Braker",
 	author_email = "linuxnit@elivecd.org",
